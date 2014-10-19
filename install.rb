b1ed79f6846140e1b22ca931e7ae0b1b2333ff40
Max <maxrogers90@gmail.com>
2014-10-19 00:25:49 -0400

    a thing

--- install.rb
+++ install.rb
@@ -1,3 +1,5 @@
 #install.rb
 `cp tuesday /usr/local/bin/tuesday`
-`chmod +x /usr/local/bin/tuesday`
+`chmod a+x /usr/local/bin/tuesday`
+`touch /usr/local/bin/kitchen`
+`chmod a+x /usr/local/bin/kitchen`
